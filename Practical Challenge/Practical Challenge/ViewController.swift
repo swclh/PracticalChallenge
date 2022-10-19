d4459c79eb6cca9f4743b614a9b17e21cec7b393
Sophie Rofe <40592658+s-rofe@users.noreply.github.com>
2022-10-19 16:09:37 +1300

    Initial Commit
    Initial project setup

---
+++ Practical Challenge/Practical Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical Challenge
+//
+//  Created by Sophie Rofe on 19/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
