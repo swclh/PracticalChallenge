b3b245c1a1ce395a9c141bb37b5ed8681784b27d
yanqizhao <yanqizhao3@gmail.com>
2022-11-24 13:00:46 +1300

    feat: add view model and data load

--- UserList/UserList/View/ULTableViewCell.swift
+++ UserList/UserList/View/ULTableViewCell.swift
@@ -9,15 +9,10 @@ import UIKit
 
 class ULTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    public func configureData(with viewModel: UserViewModel?) {
+        guard viewModel != nil else {
+            return
+        }
     }
 
 }
--- UserList/UserList/ViewModel/ULViewModel.swift
+++ UserList/UserList/ViewModel/ULViewModel.swift
@@ -6,7 +6,46 @@
 //
 
 import UIKit
+import CoreData
+
+struct UserViewModel {
+    var name: String?
+    var fullName: String?
+    var gender: String?
+    var dob: String?
+    var thumbnailImage: String?
+    var largeImage: String?
+    var phone: String?
+    var email: String?
+    var city: String?
+}
 
 class ULViewModel: NSObject {
+    
+    var fetchResults: NSFetchedResultsController<NSFetchRequestResult>!
+
+    var userCount: Int {
+        get {
+            fetchResults.fetchedObjects?.count ?? 0
+        }
+    }
+    
+    func getUser(at indexPath: IndexPath) -> UserViewModel? {
+        let user = fetchResults.object(at: indexPath) as! ULUser
+        return UserViewModel(name: user.name?.getTitleName(), fullName: user.name?.getFullName(), gender: user.gender, dob: user.dob?.date, thumbnailImage: user.picture?.thumbnail, largeImage: user.picture?.large, phone: user.phone, email: user.email, city: user.location?.city)
+    }
+    
+    func configureData(callback: @escaping () -> ()) {
+        fetchResults = NSFetchedResultsController(fetchRequest: ULUser.configure(), managedObjectContext: core_data_context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchResults.delegate = self
+        try? fetchResults.performFetch()
+        ULNetworkClient.fetchCachedData { isCached in
+            try? self.fetchResults.performFetch()
+            callback()
+        }
+    }
+}
+
+extension ULViewModel: NSFetchedResultsControllerDelegate {
 
 }
