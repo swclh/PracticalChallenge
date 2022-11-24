3b20e84eddc4344642848d0390a1eb6a3ecb35ed
yanqizhao <yanqizhao3@gmail.com>
2022-11-24 13:53:03 +1300

    feat: add detail page presentation

--- UserList/UserList/ViewController/ULDetailViewController.swift
+++ UserList/UserList/ViewController/ULDetailViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ULDetailViewController: UIViewController {
     
     var userViewModel: UserViewModel!
     
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var mailLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var dobLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let url = URL(string: userViewModel!.largeImage!)
+        avatarImageView.kf.setImage(with: url, placeholder: UIImage(named: "avatar"))
+        nameLabel.text = userViewModel.fullName
+        phoneLabel.text = userViewModel.phone
+        mailLabel.text = userViewModel.email
+        cityLabel.text = userViewModel.city
+        dobLabel.text = userViewModel.dob
     }
 }
