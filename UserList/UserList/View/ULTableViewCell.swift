e0757ef3765bcd3c9331e8e6e9e2948a65a001df
yanqizhao <yanqizhao3@gmail.com>
2022-11-24 13:16:31 +1300

    feat: add table view cell presentation

--- UserList/UserList/View/ULTableViewCell.swift
+++ UserList/UserList/View/ULTableViewCell.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ULTableViewCell: UITableViewCell {
 
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var genderLabel: UILabel!
+    @IBOutlet var dobLabel: UILabel!
+    
     public func configureData(with viewModel: UserViewModel?) {
         guard viewModel != nil else {
             return
         }
-    }
+        
+        let url = URL(string: viewModel!.thumbnailImage!)
+        avatarImageView.kf.setImage(with: url, placeholder: UIImage(named: "avatar"))
 
+        nameLabel.text = viewModel!.name
+        genderLabel.text = viewModel!.gender
+        dobLabel.text = viewModel!.dob
+    }
 }
