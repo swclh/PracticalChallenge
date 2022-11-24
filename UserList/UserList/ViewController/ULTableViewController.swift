d51b5aaabab9333a2576777af493c399b82cdb25
yanqizhao <yanqizhao3@gmail.com>
2022-11-24 13:29:16 +1300

    feat: add detail page segue

--- UserList/UserList/ViewController/ULTableViewController.swift
+++ UserList/UserList/ViewController/ULTableViewController.swift
@@ -24,6 +24,7 @@ class ULTableViewController: UITableViewController {
         tableView.mj_footer = footer
     }
     
+    // MARK: - Load more data
     @objc func footerRefresh() {
         viewModel.loadMoreData {
             self.tableView.mj_footer!.endRefreshing()
@@ -36,30 +37,29 @@ class ULTableViewController: UITableViewController {
         return viewModel.userCount
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ULTableViewCell.classString, for: indexPath) as! ULTableViewCell
         cell.configureData(with: viewModel.getUser(at: indexPath))
 
         return cell
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ULDetailViewController.classString, let cell = sender as? ULTableViewCell {
+            let destination = segue.destination as! ULDetailViewController
+            if let index = tableView.indexPath(for: cell), let userViewModel = viewModel.getUser(at: index) {
+                destination.userViewModel = userViewModel
+            }
+        }
+    }
 }
